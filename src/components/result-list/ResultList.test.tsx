58fca90a4a950cba254e0f3d197d6c291bca78e7
kumanmc <mdavid29021984@gmail.com>
2025-05-22 20:26:06 +0200

    Fix tests with priovder well mocked to havee 100% coverage

--- src/components/result-list/ResultList.test.tsx
+++ src/components/result-list/ResultList.test.tsx
@@ -5,30 +5,44 @@ import { AppProvider } from '../../context/AppContext';
 import data from '../../test-data/meals.data.json';
 import dataOne from '../../test-data/meals.one-data.json';
 
+import * as AppContextModule from '../../context/AppContext';
+jest.mock('../../context/AppContext');
+const mockedUseAppContext = AppContextModule.useAppContext as jest.Mock;
+
 describe('ResultList', () => {
 
-  test('receive no meals', () => {
+  beforeEach(() => {
+    mockedUseAppContext.mockClear();
+    mockedUseAppContext.mockReturnValue({
+      setFavoritesMode: jest.fn(),
+      favorites: [],
+      favoriteMode: false,
+      isMealFavorite: jest.fn(() => false),
+      addFavorite: jest.fn(),
+      removeFavorite: jest.fn(),
+    });
+  });
+
+  test('receive no meals', async () => {
     const setCurrentMeal = jest.fn();
     const setCurrentFavoriteMeal = jest.fn();
     render(
-      <AppProvider>
-        <ResultList
-          meals={[]}
-          userSearched={false}
-          loading={false}
-          currentMeal={null}
-          currentFavoriteMeal={null}
-          setCurrentMeal={setCurrentMeal}
-          setCurrentFavoriteMeal={setCurrentFavoriteMeal}
-        />
-      </AppProvider>
+      <ResultList
+        meals={[]}
+        userSearched={true}
+        loading={false}
+        currentMeal={null}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
     );
     const recipeList = screen.queryByRole('recipe-list');
     expect(recipeList).toBeNull();
 
-    const noResults = screen.queryByText(/No results found/i);
+    const message = screen.getByText(/No results found/i);
     waitFor(() => {
-      expect(noResults).toBeInTheDocument();
+      expect(message).toBeInTheDocument();
     });
   });
 
@@ -36,17 +50,15 @@ describe('ResultList', () => {
     const setCurrentMeal = jest.fn();
     const setCurrentFavoriteMeal = jest.fn();
     render(
-      <AppProvider>
-        <ResultList
-          meals={data.meals}
-          userSearched={false}
-          loading={false}
-          currentMeal={null}
-          currentFavoriteMeal={null}
-          setCurrentMeal={setCurrentMeal}
-          setCurrentFavoriteMeal={setCurrentFavoriteMeal}
-        />
-      </AppProvider>
+      <ResultList
+        meals={data.meals}
+        userSearched={false}
+        loading={false}
+        currentMeal={null}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
     );
 
     expect(screen.queryByText(/No results found/i)).not.toBeInTheDocument();
@@ -63,17 +75,15 @@ describe('ResultList', () => {
     const setCurrentMeal = jest.fn();
     const setCurrentFavoriteMeal = jest.fn();
     render(
-      <AppProvider>
-        <ResultList
-          meals={dataOne.meals}
-          userSearched={false}
-          loading={false}
-          currentMeal={null}
-          currentFavoriteMeal={null}
-          setCurrentMeal={setCurrentMeal}
-          setCurrentFavoriteMeal={setCurrentFavoriteMeal}
-        />
-      </AppProvider>
+      <ResultList
+        meals={dataOne.meals}
+        userSearched={false}
+        loading={false}
+        currentMeal={null}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
     );
 
     expect(screen.queryByText(/No results found/i)).not.toBeInTheDocument();
@@ -85,21 +95,20 @@ describe('ResultList', () => {
     expect(listItems).toHaveLength(1);
 
   });
+
   test('loading', async () => {
     const setCurrentMeal = jest.fn();
     const setCurrentFavoriteMeal = jest.fn();
     render(
-      <AppProvider>
-        <ResultList
-          meals={dataOne.meals}
-          userSearched={false}
-          loading={true}
-          currentMeal={null}
-          currentFavoriteMeal={null}
-          setCurrentMeal={setCurrentMeal}
-          setCurrentFavoriteMeal={setCurrentFavoriteMeal}
-        />
-      </AppProvider>
+      <ResultList
+        meals={dataOne.meals}
+        userSearched={false}
+        loading={true}
+        currentMeal={null}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
     );
 
     expect(screen.queryByText(/No results found/i)).not.toBeInTheDocument();
@@ -110,5 +119,105 @@ describe('ResultList', () => {
 
   });
 
+  test('go back to search from View favorite details', async () => {
+    const setCurrentMeal = jest.fn();
+    const setCurrentFavoriteMeal = jest.fn();
+    const favoriteMealMock = dataOne.meals[0];
+
+    render(
+      <ResultList
+        meals={null}
+        userSearched={false}
+        loading={false}
+        currentMeal={null}
+        currentFavoriteMeal={favoriteMealMock}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
+    );
+
+    waitFor(() => {
+      expect(screen.queryByText(favoriteMealMock.strMeal)).toBeInTheDocument();
+      expect(screen.queryByRole('button', { name: /Back/i })).toBeInTheDocument();
+
+    });
+
+
+    const backButton = screen.getByRole('button', { name: /Back/i });
+    fireEvent.click(backButton);
+
+    waitFor(() => {
+      expect(setCurrentFavoriteMeal).toHaveBeenCalledTimes(1);
+      expect(setCurrentFavoriteMeal).toHaveBeenCalledWith(null);
+
+      expect(screen.queryByRole('list')).not.toBeInTheDocument();
+    });
+
+  });
+
+
+  test('go back to search from View details', async () => {
+    const setCurrentMeal = jest.fn();
+    const setCurrentFavoriteMeal = jest.fn();
+    const currentMealMock = dataOne.meals[0];
+
+    render(
+      <ResultList
+        meals={null}
+        userSearched={false}
+        loading={false}
+        currentMeal={currentMealMock}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
+    );
+
+    expect(screen.getByText(currentMealMock.strMeal)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Back/i })).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: /Back/i }));
+    expect(setCurrentMeal).toHaveBeenCalledTimes(1);
+    expect(setCurrentMeal).toHaveBeenCalledWith(null);
+
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
+  });
+
+  test('should display Favorites component when favoriteMode is true', async () => {
+    const setCurrentMeal = jest.fn();
+    const setCurrentFavoriteMeal = jest.fn();
+
+    mockedUseAppContext.mockReturnValue({
+      setFavoritesMode: jest.fn(),
+      favorites: [],
+      favoriteMode: true,
+      isMealFavorite: jest.fn(() => false),
+      addFavorite: jest.fn(),
+      removeFavorite: jest.fn(),
+    });
+
+    render(
+      <ResultList
+        meals={null}
+        userSearched={false}
+        loading={false}
+        currentMeal={null}
+        currentFavoriteMeal={null}
+        setCurrentMeal={setCurrentMeal}
+        setCurrentFavoriteMeal={setCurrentFavoriteMeal}
+      />
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText('No favorites found')).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /Back to search/i })).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByRole('button', { name: /Back to search/i }));
+    expect(mockedUseAppContext().setFavoritesMode).toHaveBeenCalledTimes(1);
+    expect(mockedUseAppContext().setFavoritesMode).toHaveBeenCalledWith(false);
+
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
+  });
 
 })
