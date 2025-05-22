a755c57987144e2c5fb78c1e66c1be71d4853e5e
kumanmc <mdavid29021984@gmail.com>
2025-05-22 16:06:23 +0200

    Extract Favorites

---
+++ src/components/favorites/Favorites.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
+import { Meal } from '../../types/meal.type';
+import MealCard from '../meal-card/MealCard';
+import { useAppContext } from '../../context/AppContext';
+
+interface FavoritesProps {
+  setCurrentMeal: (meal: Meal) => void;
+}
+
+const Favorites: React.FC<FavoritesProps> = ({ setCurrentMeal }) => {
+  const { setFavoritesMode, favorites } = useAppContext();
+
+  const handleBackToSearch = () => {
+    setFavoritesMode(false);
+  };
+
+  return (
+    <>
+      <Row>
+        <Col xs={12} className='text-center'>
+          <h2>Favorites</h2>
+        </Col>
+      </Row>
+      {
+        favorites && favorites.length > 0 && (
+          <Row role='list' className="border p-3 rounded">
+            {favorites.map((meal) => (
+              <Col role={'listitem'} key={meal.idMeal} xs={12} sm={6} md={4} lg={3} className="mb-2 d-flex">
+                <MealCard meal={meal} onViewDetails={setCurrentMeal} />
+              </Col>
+            ))}
+          </Row>
+        )
+      }
+      <Row>
+        <Col className={'col-12'}>
+          <Button
+            variant="primary"
+            onClick={handleBackToSearch}
+            aria-label={`Back to Search`}
+          >
+            Back to search
+          </Button>
+        </Col>
+      </Row>
+
+    </>
+  );
+};
+
+export default Favorites;
--- src/components/result-list/ResultList.tsx
+++ src/components/result-list/ResultList.tsx
@@ -5,6 +5,7 @@ import { Row, Col, Alert, Button } from 'react-bootstrap';
 import ViewDetails from '../view-details/ViewDetails';
 import { useAppContext } from '../../context/AppContext';
 import { Star, StarFill } from 'react-bootstrap-icons';
+import Favorites from '../favorites/Favorites';
 
 interface ResultListProps {
   meals: Meal[] | null;
@@ -13,73 +14,37 @@ interface ResultListProps {
 
 const ResultList: React.FC<ResultListProps> = ({ meals, userSearched }) => {
   const [currentMeal, setCurrentMeal] = useState<Meal | null>(null);
-  const { favoriteMode, setFavoritesMode, favorites } = useAppContext();
+  const { favoriteMode } = useAppContext();
 
   const handleOnGoBack = () => {
     setCurrentMeal(null);
-  };
-  const handleBackToSearch = () => {
-    setFavoritesMode(false);
   };
 
   return (
     <>
       {
-        favoriteMode ? (
-          <>
-            <Row>
-              <Col xs={12} className='text-center'>
-                <h2>Favorites</h2>
-              </Col>
-            </Row>
-            {
-              favorites && favorites.length > 0 && (
+        favoriteMode ? (<Favorites setCurrentMeal={setCurrentMeal} />) :
+          currentMeal ? (<ViewDetails meal={currentMeal} onGoBack={handleOnGoBack} />) :
+            meals && meals.length > 0 ?
+              (
                 <Row role='list' className="border p-3 rounded">
-                  {favorites.map((meal) => (
+                  {meals.map((meal) => (
                     <Col role={'listitem'} key={meal.idMeal} xs={12} sm={6} md={4} lg={3} className="mb-2 d-flex">
                       <MealCard meal={meal} onViewDetails={setCurrentMeal} />
                     </Col>
                   ))}
                 </Row>
               )
-            }
-            <Row>
-              <Col className={'col-12'}>
-                <Button
-                  variant="primary"
-                  onClick={handleBackToSearch}
-                  aria-label={`Back to Search`}
-                >
-                  Back to search
-                </Button>
-              </Col>
-            </Row>
-
-          </>
-        ) : currentMeal ?
-          (
-            <ViewDetails meal={currentMeal} onGoBack={handleOnGoBack} />
-          )
-          : meals && meals.length > 0 ?
-            (
-              <Row role='list' className="border p-3 rounded">
-                {meals.map((meal) => (
-                  <Col role={'listitem'} key={meal.idMeal} xs={12} sm={6} md={4} lg={3} className="mb-2 d-flex">
-                    <MealCard meal={meal} onViewDetails={setCurrentMeal} />
-                  </Col>
-                ))}
-              </Row>
-            )
-            : userSearched &&
-            (
-              <Row className="align-items-center">
-                <Alert variant='warning' >
-                  <Alert.Heading>No results found</Alert.Heading>
-                  Try with other ingredients or keywords.
-                </Alert>
-                <h2></h2>
-              </Row>
-            )
+              : userSearched &&
+              (
+                <Row className="align-items-center">
+                  <Alert variant='warning' >
+                    <Alert.Heading>No results found</Alert.Heading>
+                    Try with other ingredients or keywords.
+                  </Alert>
+                  <h2></h2>
+                </Row>
+              )
 
       }
     </>
